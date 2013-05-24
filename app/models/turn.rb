47029f85a3d1f0019c20ea5de4e358a2d613bbe1
Alex Fogg <me@alexfogg.com>
2013-05-23 22:12:11 -0400

    more work on the turn scoring methods

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -25,11 +25,15 @@ class Turn < ActiveRecord::Base
   end
 
   def calculate_points #score calculation function for each possible combo
+    @numbers = calculate_numbers(@dice)
+
    # straight 1-2-3-4-5-6
-    if
+    if @numbers.sort == [1,2,3,4,5,6]
+      @points = 1500
+    elsif
 
-   # single "1" die
-
+    # single "1" die
+    elsif
     # single "5" die
 
     # three "1" dice
@@ -56,17 +60,21 @@ class Turn < ActiveRecord::Base
 
   end
 
-  def calculate_dice #
+  def calculate_numbers(dice) #turns the dice into an array of individual values
     i = 0
-    @dice.count.times do |i|
-
-      # until i = 5
-      #   i=i+1
-      # end
+    dice.count.times do |i|
+      numbers[i]=@dice[i].value
+      i=i+1
     end
+    return numbers
   end
 
-  def sort_dice #sorts the array of dice?
+  def duplicates(dice) #check to see if there are matching dice
+    group = []
+    for i in 1..6
+      group[i-1] = dice.count(i)
+    end
+    return group
   end
 
 end
